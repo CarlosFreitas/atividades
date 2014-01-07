fd83e74a88554bbb1f9b0361adaa586a616e03d5
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-07 18:29:44 -0200

    Create editar.php

---
+++ cadastro/editar.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    
+    if(!isset($_SESSION["cadastros"])){
+        echo "Não existem pessoas para editar";  
+    }
+    else{
+        $id = $_REQUEST["id"];
+        $nome = $_REQUEST["nome"];
+        
+        $cadastros =& $_SESSION["cadastros"];
+        $cadastros[$id] = $nome;
+        
+        echo "Edição efetuada com sucesso!";
+    }    
+    
+    echo "Cadastro efetuado com sucesso!";
+   
+?>
