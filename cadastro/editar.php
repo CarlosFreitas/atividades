ecf4aa317b53d74542a9e8150aca5d06805d91f6
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-10 15:17:06 -0200

    Update editar.php

--- cadastro/editar.php
+++ cadastro/editar.php
@@ -9,9 +9,23 @@
     else{
         $id = $_REQUEST["id"];
         $nome = $_REQUEST["nome"];
+        $sexo = $_REQUEST["sexo"];
+        $estado = $_REQUEST["estado"];
+        
+        $aceito = false;
+    if(isset($_REQUEST["aceito"])){
+        $aceito = true;
+    }
+    
+        
+        $pessoa = array();
+        $pessoa["nome"] = $nome;
+        $pessoa["sexo"] = $sexo;
+        $pessoa["aceito"] = $aceito;
+        $pessoa["estado"] = $estado;
         
         $cadastros =& $_SESSION["cadastros"];
-        $cadastros[$id] = $nome;
+        $cadastros[$id] = $pessoa;
         
         echo "Edição efetuada com sucesso!";
     }    
