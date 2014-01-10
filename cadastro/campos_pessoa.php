cf12007e48af21910250b9bc696b02764e207510
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-10 15:21:05 -0200

    Create campos_pessoa.php

---
+++ cadastro/campos_pessoa.php
@@ -0,0 +1,18 @@
+<label>Nome: </label>
+ <input type="text" name="nome" />
+ <br/>
+ <input type="radio" name="sexo" value="m" checked="checked" />
+ <label>Masculino</label>
+ <input type="radio" name="sexo" value="f" />
+ <label>Feminino</label>
+ <br/>
+ <input type="checkbox" name="aceito" value="sim" />
+ <label>Aceito o contrato</label>
+ <br/>
+ <label>Estado</label>
+ <select name="estado">
+    <option>Bahia</option>
+    <option>São Paulo</option>
+    <option>Rio de Janeiro</option>
+ </select>
+ <br/>
