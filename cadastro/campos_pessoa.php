897437069555753d9cba6c6e2b98a195e94c378d
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-10 16:09:40 -0200

    Update campos_pessoa.php

--- cadastro/campos_pessoa.php
+++ cadastro/campos_pessoa.php
@@ -1,18 +1,23 @@
 <label>Nome: </label>
- <input type="text" name="nome" />
- <br/>
- <input type="radio" name="sexo" value="m" checked="checked" />
- <label>Masculino</label>
- <input type="radio" name="sexo" value="f" />
- <label>Feminino</label>
- <br/>
- <input type="checkbox" name="aceito" value="sim" />
- <label>Aceito o contrato</label>
- <br/>
- <label>Estado</label>
- <select name="estado">
+<input type="text" name="nome" />
+<br/>
+<input type="radio" name="sexo" value="m" checked="checked" />
+<label>Masculino</label>
+<input type="radio" name="sexo" value="f" />
+<label>Feminino</label>
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Aceito o contrato</label>
+<br/>
+<label>Estado</label>
+<select name="estado">
     <option>Bahia</option>
     <option>São Paulo</option>
     <option>Rio de Janeiro</option>
- </select>
- <br/>
+</select>
+<br/>
+<label>Observações</label>
+<br/>
+<textarea name="observacoes" rows="10"cols="20" ></textarea>
+<br/>
+ 
