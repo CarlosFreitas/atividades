107464ceadc81ea7f302bbe2a4155ef6ee3a6f49
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-13 18:57:19 -0200

    Create campos_produtos.php

---
+++ produto/campos_produtos.php
@@ -0,0 +1,27 @@
+<label>Nome: </label>
+<input type="text" name="nome" />
+<br/>
+<input type="radio" name="cor" value="a" checked="checked" />
+<label>Azul</label>
+<br/>
+<input type="radio" name="cor" value="v" checked="checked" />
+<label>Vermelho</label>
+<br/>
+<input type="radio" name="cor" value="r" checked="checked" />
+<label>Rosa</label>
+<input type="checkbox" name="Produto Novo" value="Sim" />
+<label>Produto Aceito</label>
+<br/>
+<label>Prazo de Entrega</label>
+<select name="opções">
+    <option>5 Dias</option>
+    <option>10 Dias</option>
+    <option>15 Dias</option>
+</select>
+<br/>
+<br/>
+<label>Observações</label>
+<br/>
+<textarea name="observacoes" rows="10"cols="20" ></textarea>
+<br/>
+ 
