6bd42af8158bd950ac00adab63c95d8b48adc9b8
CarlosFreitas <c7_freitas@hotmail.com>
2014-01-13 18:54:17 -0200

    Create form_cadastrar.php

---
+++ produto/form_cadastrar.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Cadastro de Produtos</title>
+    </head>
+    
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post">
+            <?php require_once("campos_produtos.php"); ?>
+            <input type="submit" value="Enviar" />
+        </form>
+    </body>
+</html>
